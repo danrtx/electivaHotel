8a76ceb29b54a8991060acc9f572ee5e19d6d6e3
danrtx <danilo.montezumaib@campusucc.edu.co>
2025-04-24 20:45:46 -0500

    testsv1

---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+// jest.setup.js
+console.log('jest.setup.js is being loaded')
+import '@testing-library/jest-dom'         // ESModules
+// ──o si prefieres CommonJS:
+// require('@testing-library/jest-dom')
